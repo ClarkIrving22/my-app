bf4d2756829286ebfa077df861bfe2b764821d4c
Clark Irving Isla Balarezo <clark.irving@icloud.com>
2023-02-15 17:37:36 -0500

    eh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          ClarkIrving22
+          ClarkIrving22 eeeh
         </p>
         <a
           className="App-link"
